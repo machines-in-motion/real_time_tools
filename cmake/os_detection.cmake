f88dfbca0c29b109c39098762ba2aa97870b4a36
Maximilien Naveau <maximilien.naveau@gmail.com>
2018-09-25 19:10:26 +0200

    fix the os detection. TODO: use the flags in the code to create different real_time_thread

--- cmake/os_detection.cmake
+++ cmake/os_detection.cmake
@@ -6,8 +6,8 @@
 #
 macro(DEFINE_OS)
   #
-  set(CURRENT_OS "Ubuntu" CACHE STRING
-      "Set it to \"xenomai\" or \"preempt-rt\" or \"Ubuntu\" to specify the OS")
+  set(CURRENT_OS "ubuntu" CACHE STRING
+      "Set it to \"xenomai\" or \"rt-preempt\" or \"ubuntu\" to specify the OS")
   # Update submodules as needed
   execute_process(
       COMMAND uname --all
@@ -17,14 +17,16 @@ macro(DEFINE_OS)
     set(CURRENT_OS "xenomai")
     add_definitions("-DXENOMAI")
   elseif(OS_KERNEL_VERSION MATCHES "preempt-rt")
+    set(CURRENT_OS "rt-preempt")
     add_definitions("-DRT_PREEMPT")
   elseif(OS_KERNEL_VERSION MATCHES "Ubuntu")
-    set(CURRENT_OS "Ubuntu")
+    set(CURRENT_OS "ubuntu")
     add_definitions("-DUBUNTU")
   else()
     message(FATAL_ERROR "Could not detect the OS version please "
       "fix os_detection.cmake")
   endif()
   #
+  message(STATUS "OS found is " ${CURRENT_OS})
 endmacro(DEFINE_OS)
 
