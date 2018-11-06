ba76c14b7798f193740a9e202d900f58741fc121
Naveau <maximilien.naveau@gmail.com>
2018-11-06 20:23:05 +0100

    remove the build folder

--- _build-qt-Release/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/mnaveau/devel/workspace/src/catkin/system/real_time_tools/_build-qt-Release/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
