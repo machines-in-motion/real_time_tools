943f42f6a176e594675fa4835ddf18f4c9e2a1f4
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-05-22 17:13:47 +0200

    fix some header and the license name

--- include/real_time_tools/rt_mutex.hpp
+++ include/real_time_tools/rt_mutex.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file rt_mutex.hpp
+ * @author Maximilien Naveau (maximilien.naveau@gmail.com)
+ * @license License BSD-3-Clause
+ * @copyright Copyright (c) 2019, New York University and Max Planck Gesellshaft.
+ * @date 2019-05-22
+ * 
+ * @brief Expose real time OSs mutex through a common API.
+ */
 #ifdef __XENO__
  #include <native/mutex.h>
  #include <native/cond.h>
