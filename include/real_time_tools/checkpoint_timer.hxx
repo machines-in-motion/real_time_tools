1139b609a8ff37dd51d8f83a0319f8184fb85c5b
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-01-15 12:29:00 +0100

    Add CheckpointTimer to simplify time measurement in loops
    The `CheckpointTimer` class is intended for time measurement of loops
    where the time spent in separate sections of the code is measured, as
    well as the total duration of the loop.  See doxygen of the class for
    more details.
    
    It is basically a wrapper around a list of `Timer` instances with the
    purpose to not clutter the measured code too much.  Also it can easily
    be disabled via a template argument which should result in the compiler
    optimizing away the function calls completely (i.e. no need to have
    commented lines in the code).

---
+++ include/real_time_tools/checkpoint_timer.hxx
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * @license BSD 3-clause
+ * @copyright Copyright (c) 2020, New York University and Max Planck
+ *            Gesellschaft
+ *
+ * @brief Implementation of the CheckpointTimer class.
+ */
+
+template <size_t NUM_CHECKPOINTS, bool ENABLED>
+CheckpointTimer<NUM_CHECKPOINTS, ENABLED>::CheckpointTimer()
+{
+    static_assert(NUM_CHECKPOINTS > 0,
+                  "CheckpointTimer needs at least one checkpoint");
+    checkpoint_names_[0] = "Total";
+}
+
+template <size_t NUM_CHECKPOINTS, bool ENABLED>
+void CheckpointTimer<NUM_CHECKPOINTS, ENABLED>::start()
+{
+    if (ENABLED)
+    {
+        timers_[0].tac_tic();
+        current_checkpoint = 1;
+        timers_[current_checkpoint].tic();
+    }
+}
+
+template <size_t NUM_CHECKPOINTS, bool ENABLED>
+void CheckpointTimer<NUM_CHECKPOINTS, ENABLED>::checkpoint(
+    const std::string &checkpoint_name)
+{
+    if (ENABLED)
+    {
+        timers_[current_checkpoint].tac();
+
+        if (checkpoint_names_[current_checkpoint].empty())
+        {
+            checkpoint_names_[current_checkpoint] = checkpoint_name;
+        }
+        else if (checkpoint_names_[current_checkpoint] != checkpoint_name)
+        {
+            throw std::runtime_error("Wrong checkpoint called (expected '" +
+                                     checkpoint_names_[current_checkpoint] +
+                                     "' but got '" + checkpoint_name + "').");
+        }
+
+        current_checkpoint++;
+        if (current_checkpoint < timers_.size())
+        {
+            timers_[current_checkpoint].tic();
+        }
+    }
+}
+
+template <size_t NUM_CHECKPOINTS, bool ENABLED>
+void CheckpointTimer<NUM_CHECKPOINTS, ENABLED>::print_statistics() const
+{
+    if (ENABLED)
+    {
+        std::cout << "======================================" << std::endl;
+        for (size_t i = 0; i < timers_.size(); i++)
+        {
+            std::cout << "===== " << checkpoint_names_[i] << std::endl;
+            timers_[i].print_statistics();
+        }
+    }
+}
