debb175b8aa88d5786f53f209c85da7fd697ce41
Naveau <maximilien.naveau@gmail.com>
2021-03-02 10:44:57 +0100

    use pure cmake to build and install the package (remove ament) (#25)
    * use pure cmake to build and install the package (remove ament)
    
    * [format]
    
    * upgrade version to 2.0.0
    
    * fix export of dependencies

--- src/bin/compute_statistics.py
+++ src/bin/compute_statistics.py
@@ -23,19 +23,23 @@ from os import listdir
 from os.path import isfile, join
 from time import localtime, strftime
 
+
 def _get_all_dat_files(folder):
-    return [f for f in listdir(folder)
-            if (isfile(join(folder, f)) and len(f) >= 4 and '.dat' in f[-4:])]
+    return [
+        f
+        for f in listdir(folder)
+        if (isfile(join(folder, f)) and len(f) >= 4 and ".dat" in f[-4:])
+    ]
 
 
 def _get_data_from_file(in_file_name):
-    data=[]
-    with open(in_file_name, 'rb') as in_file:
+    data = []
+    with open(in_file_name, "rb") as in_file:
         for row in in_file:
             txt_row = row.split()
             float_row = [float(i) for i in txt_row]
             data.append(float_row)
-    return np.matrix(data)[:,1:]
+    return np.matrix(data)[:, 1:]
 
 
 def _compute_statitics(data):
@@ -44,9 +48,9 @@ def _compute_statitics(data):
     _mean = []
     _std_dev = []
     for i in range(data.shape[1]):
-        _min    .append(np.min(data[:, i]))
-        _max    .append(np.max(data[:, i]))
-        _mean   .append(np.mean(data[:, i]))
+        _min.append(np.min(data[:, i]))
+        _max.append(np.max(data[:, i]))
+        _mean.append(np.mean(data[:, i]))
         _std_dev.append(np.std(data[:, i]))
     return _min, _max, _mean, _std_dev
 
@@ -58,15 +62,20 @@ def main(sys_args):
         "The files must possess at least 2 columns. The statistics are going "
         "to be computed from the econd one. The typical use is to give this "
         "executable a folder path and it will check all files with an "
-        "extension \".dat\" and perform the statistics")
+        'extension ".dat" and perform the statistics'
+    )
 
     parser = argparse.ArgumentParser(description=description_str)
-    parser.add_argument('--folder', metavar='in_dir', type=str,
-                        help='folder where to look for the *.dat files')
+    parser.add_argument(
+        "--folder",
+        metavar="in_dir",
+        type=str,
+        help="folder where to look for the *.dat files",
+    )
     args = parser.parse_args(sys_args)
 
     # get files path
-    if 'in_dir' in args:
+    if "in_dir" in args:
         in_dir = args.in_dir
     else:
         in_dir = "."
@@ -80,21 +89,26 @@ def main(sys_args):
         _min, _max, _mean, _std_dev = _compute_statitics(data)
         # prepare the output
         output += (
-                "The parsed file is: " + in_file + "\n"
-                "The computed statistics are the following:\n"
-                "    - min: " + str(_min)   + "\n"
-                "    - max: " + str(_max)   + "\n"
-                "    - mean: " + str(_mean) + "\n"
-                "    - standard deviation: " + str(_std_dev)) + "\n\n"
+            "The parsed file is: " + in_file + "\n"
+            "The computed statistics are the following:\n"
+            "    - min: " + str(_min) + "\n"
+            "    - max: " + str(_max) + "\n"
+            "    - mean: " + str(_mean) + "\n"
+            "    - standard deviation: " + str(_std_dev)
+        ) + "\n\n"
 
-    out_file_name = join(in_dir, "statistics_results_" +
-                    strftime("%Y_%m_%d_%H_%M_%S", localtime()) + ".txt")
+    out_file_name = join(
+        in_dir,
+        "statistics_results_"
+        + strftime("%Y_%m_%d_%H_%M_%S", localtime())
+        + ".txt",
+    )
 
     print(output)
-    with open(out_file_name, 'w') as out_file:
+    with open(out_file_name, "w") as out_file:
         out_file.write(output)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
     sys.exit(0)
-
